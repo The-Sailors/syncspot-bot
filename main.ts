de15aa5a64f9ba0b64e93381338fd9cd0d3077c4
jojo <perebaj@gmail.com>
2024-03-28 15:25:40 -0300

    :sparkles: remove raise hand

--- main.ts
+++ main.ts
@@ -39,9 +39,6 @@ puppeteer.use(StealthPlugin());
     const askToJoin = await page.waitForSelector('#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb > div:nth-child(1) > button')
     await askToJoin?.click()
 
-    const raiseHandButton = await page.waitForSelector('#yDmH0d > c-wiz > div > div > div:nth-child(24) > div.crqnQb > div.fJsklc.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div > div:nth-child(6) > div > div.p2SYhf > span > button')
-    await raiseHandButton?.click()
-
     const chatWithEveryOneButton = await page.waitForSelector('#yDmH0d > c-wiz > div > div > div:nth-child(24) > div.crqnQb > div.fJsklc.nulMpf.Didmac.G03iKb > div > div > div.jsNRx > div > div:nth-child(3) > div > div > span > button')
     await chatWithEveryOneButton?.click({ delay: 1000 }) // delay is added to wait for the chat drawler to open
 
